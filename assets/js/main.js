58d22d5a392e1bf8a5ac4892a17609218625fcfb
rayan <desmetrayan621@gmail.com>
2025-03-11 12:31:15 +0100

    rectification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-// question
-let mesQuestions = [
+// question (ortographe a verifier)
+let Questions = [
     "Qui est le père de Thor ?",
     "Quel est le véritable nom de l'Iron Man ?",
     "Dans quel film Spider-Man rencontre-t-il Iron Man pour la première fois ?",
@@ -11,8 +11,8 @@ let mesQuestions = [
     "Quel est le nom de l'arme de Thor, qui peut invoquer des éclairs et est aussi un symbole de son pouvoir ?",
     "Comment se nomme la race des êtres cosmiques responsables de la création des Eternals ?"
 ];
-// les reponse
-let reponsesCorrectes = [
+// les reponse (ortographe a verifier)
+let reponses = [
     "odin", 
     "tony stark", 
     "captain america civil war", 
@@ -30,11 +30,12 @@ let reponsesCorrectes = [
 let score = 0;
 
 // qstn reponse test v1.4
-for (let i = 0; i < mesQuestions.length; i++) {
-    let reponse = prompt(mesQuestions[i]);             // permet dafficher les questions une par une en prompt(affichage style pup) et de rentrer une reponse
+
+for (let i = 0; i < Questions.length; i++) {
+    let reponse = prompt(Questions[i]);             // permet dafficher les questions une par une en prompt(affichage style pup) et de rentrer une reponse
 
 
-    if (reponse === reponsesCorrectes[i]) {
+    if (reponse === reponses[i]) {
         console.log("Bonne réponse !");               //dans cette boucle j'ai demander que a chaque bonne réponse la console affiche bonne reponse ce qui ferait un point en plus
         score++;  
 
