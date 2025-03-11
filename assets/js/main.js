ef1dea7871dc6a263917744a85dfb02a62594429
rayan <desmetrayan621@gmail.com>
2025-03-11 11:37:37 +0100

    add liste

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let mesQuestions = [
+    "Qui est le père de Thor ?",
+    "Quel est le véritable nom de l'Iron Man ?",
+    "Dans quel film Spider-Man rencontre-t-il Iron Man pour la première fois ?",
+    "Quel est le nom de l'alter ego de Hulk ?",
+    "Quel est le nom de l'Organisation qui veut contrôler le monde dans le film Captain America: The Winter Soldier ?",
+    "Qui est le méchant principal dans le film Avengers: Infinity War ?",
+    "Quel est le nom de l'arme utilisée par Captain America ?",
+    "Qui est l'antagoniste principal dans 'Black Panther' ?",
+    "Quel est le nom de l'arme de Thor, qui peut invoquer des éclairs et est aussi un symbole de son pouvoir ?",
+    "Comment se nomme la race des êtres cosmiques responsables de la création des Eternals ?"
+];
